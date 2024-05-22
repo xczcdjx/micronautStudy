f46c9e49405b2bc42b17ca7233868b15fd6e9749
小丑在唱独角戏 <10097251+xczcdjx@user.noreply.gitee.com>
2024-05-22 22:20:17 +0800

    查询

--- src/main/kotlin/com/example/controller/hi.kt
+++ src/main/kotlin/com/example/controller/hi.kt
@@ -1,5 +1,6 @@
 package com.example.controller
 
+import io.micronaut.core.annotation.Introspected
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
@@ -22,20 +23,25 @@ object User : Table<Nothing>("user") {
     val name = varchar("name")
     val sex = varchar("sex")
 }
+
+@Introspected
 data class ReUser(val id:Int,val name:String,val sex:String)
+@Introspected
+data class ResU(val users:List<ReUser>)
 @Tag(name = "Todo")
 @Controller("/todo")
 open class HelloController(@Inject private val con:Database) {
     @Get(uri = "/hello")
     @Operation(summary = "test", description = "hello")
     @Produces(MediaType.APPLICATION_JSON)
-    open fun index(): List<ReUser> {
-        return con.from(User).select().map { row ->
+    open fun index(): ResU {
+        val res=con.from(User).select().map { row ->
             ReUser(
                 id = row[User.id]!!,
                 name = row[User.name]!!,
                 sex = row[User.sex]!!
             )
         }
+        return ResU(res)
     }
 }
